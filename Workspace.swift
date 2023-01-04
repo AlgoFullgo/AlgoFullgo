3d4ca0040475faffeb30c5d0e26621a4c4f09590
woookdev <hjw0911@gmail.com>
2023-01-05 03:59:54 +0900

    feat: 기존 Project 파일을 제거 하고 workspace로 수정

---
+++ Workspace.swift
@@ -0,0 +1,14 @@
+//
+//  Workspace.swift
+//  AlgoFullgoManifests
+//
+//  Created by Jaewook Hwang on 2023/01/05.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let workspace = Workspace(
+    name: "AlgoFullgo",
+    projects: ["Projects/AlgoFullgo"]
+)
