36165bc5f511194ea94cca3d33bc5f2aa4effe0a
junhyeon.oh <oh.junhyeon95@gmail.com>
2023-01-09 12:56:18 +0900

    첫번째 뷰 세팅

--- Projects/AlgoFullgo/Targets/AlgoFullgoCreate/Sources/RegisterQuizSuccessView.swift
+++ Projects/AlgoFullgo/Targets/AlgoFullgoCreate/Sources/RegisterQuizSuccessView.swift
@@ -18,19 +18,45 @@ struct RegisterQuizSuccessView: View {
                         .font(.system(size: 20, weight: .medium, design: .default))
                     Spacer()
                 }
-                .padding(EdgeInsets(top: 40, leading: 20, bottom: 0, trailing: 0))
+                .padding(EdgeInsets(top: 40, leading: 0, bottom: 0, trailing: 0))
 
-                Button("해결했어요 👍") {
-                    print("222")
+                Button {
+                    print("aaa")
+                } label: {
+                    HStack {
+                        Text("해결했어요 👍")
+                        Spacer()
+                        Image(systemName: "checkmark")
+                            .opacity(1)
+                    }
                 }
-                .buttonStyle(.bordered)
+                .buttonStyle(QuizButtomStyle())
 
-                Button("다음에 다시 풀래요 😅") {
-                    print("123")
+                Button {
+                    print("bbb")
+                } label: {
+                    HStack {
+                        Text("다음에 다시 풀래요 😅")
+                        Spacer()
+                        Image(systemName: "checkmark")
+                            .opacity(1)
+                    }
                 }
+                .buttonStyle(QuizButtomStyle())
 
                 Spacer()
+                HStack {
+                    Button {
+
+                    } label: {
+                        Text("123213")
+                            .foregroundColor(Color.white)
+                            .background(Color.red)
+                            .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
+                    }
+                }
             }
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
             .navigationBarTitle(
                 Text("결과 등록"),
                 displayMode: .inline
@@ -41,6 +67,23 @@ struct RegisterQuizSuccessView: View {
                 Image(systemName: "xmark")
                     .tint(Color.gray)
             }))
+        }
+    }
+}
+
+struct QuizButtomStyle: ButtonStyle {
+    var isSelected: Bool = false
+
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.label
+                .padding()
+                .foregroundColor(isSelected ? Color.black : Color.gray)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(isSelected ? Color.black : Color.gray, lineWidth: 1)
+                )
+            Spacer()
         }
     }
 }
