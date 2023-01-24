a83170e85ff71f3ff60fb4d291320215f8eca98a
woookdev <hjw0911@gmail.com>
2023-01-25 04:43:22 +0900

    feat: MainTabView 기본 구현 추가

---
+++ Projects/AlgoFullgo/Targets/AlgoFullgoMain/Sources/Views/MainTabView.swift
@@ -0,0 +1,59 @@
+//
+//  MainTabView.swift
+//  AlgoFullgoMain
+//
+//  Created by Jaewook Hwang on 2023/01/24.
+//  Copyright © 2023 algofullgo.com. All rights reserved.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+public struct MainTabView: View {
+  let store: StoreOf<MainTabFeature>
+  
+  public init(store: StoreOf<MainTabFeature>) {
+    self.store = store
+  }
+  
+  public var body: some View {
+
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      TabView(
+        selection: viewStore.binding(
+          get: \.selectedTab,
+          send: .selectTab
+        )
+      ) {
+        MainView(
+          store: StoreOf<MainFeature>(
+            initialState: MainFeature.State(),
+            reducer: MainFeature()
+          )
+        )
+        .tabItem {
+          Image("tab_main_list_fill_icon")
+          Text("리스트")
+        }
+        
+        MainView(
+          store: StoreOf<MainFeature>(
+            initialState: MainFeature.State(),
+            reducer: MainFeature()
+          )
+        )
+          .tabItem {
+            Image("tab_settings_empty_icon")
+            Text("설정")
+          }
+      }
+    }
+  }
+}
+
+/*struct MainTabView_Previews: PreviewProvider {
+  static var previews: some View {
+    MainTabView(store: Store<MainTabBarState, MainTabBarAction>.init(initialState: MainTabBarState.init(selectedTab: .main), reducer: mainTabBarReducer))
+  }
+}*/
