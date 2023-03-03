d470bfc6a1f5d977ee3e4f518367f3cde5ed5ca6
woookdev <hjw0911@gmail.com>
2023-03-04 02:06:38 +0900

    feat: 디자인 시스템을 위한 color 추가

---
+++ Projects/AlgoFullgo/Targets/AlgoFullgoCore/Sources/Constants/AlgoFullgoColors.swift
@@ -0,0 +1,47 @@
+//
+//  AlgoFullgoColors.swift
+//  AlgoFullgoMain
+//
+//  Created by Jaewook Hwang on 2023/02/08.
+//  Copyright © 2023 algofullgo.com. All rights reserved.
+//
+
+import SwiftUI
+
+public extension Color {
+  static let lightAlgoGray = Color(
+    red: 241 / 255,
+    green: 241 / 255,
+    blue: 241 / 255
+  )
+  
+  static let deepAlgoGray = Color(
+    red: 172 / 255,
+    green: 172 / 255,
+    blue: 172 / 255
+  )
+  
+  static let normalAlgoGray = Color(
+    red: 199 / 255,
+    green: 208 / 255,
+    blue: 214 / 255
+  )
+  
+  static let textGray = Color(
+    red: 127 / 255,
+    green: 137 / 255,
+    blue: 153 / 255
+  )
+  
+  static let darkishBlue = Color(
+    red: 44 / 255,
+    green: 55 / 255,
+    blue: 80 / 255
+  )
+  
+  static let darkishDeepBlue = Color(
+    red: 39 / 255,
+    green: 46 / 255,
+    blue: 62 / 255
+  )
+}
